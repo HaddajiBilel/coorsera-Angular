3b5d30f896a31a3385374de7ff6cd95db2fd3c11
HaddajiBilel <haddajibilel@outlook.com>
2020-03-09 16:50:22 +0100

    promises

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -19,6 +19,7 @@ import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms'; 
 import { MatSelectModule } from '@angular/material/select';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 //Components
 import { MenuComponent } from './menu/menu.component';
@@ -68,6 +69,7 @@ import { LoginComponent } from './login/login.component';
     ReactiveFormsModule,
     MatSelectModule,
     MatSlideToggleModule,
+    MatProgressSpinnerModule
 
   ],
   providers: [
